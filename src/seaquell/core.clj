acf936b0381a53a91b754777eccb79e975a41879
David Kettering <david.ringman@gmail.com>
2013-02-17 09:13:01 -0600

    Factor out to-sql to its own file

--- src/seaquell/core.clj
+++ src/seaquell/core.clj
@@ -1,7 +1,6 @@
 (ns seaquell.core
   (:use diesel.core)
-  (:require [clojure.string :as string])
-  (:require [seaquell.engine :as eng]))
+  (:require [seaquell [to-sql :as sql] [engine :as eng]]))
 
 (def-props as from limit offset where)
 
@@ -32,35 +31,9 @@
 (defn desc [& xs]
   {:order :desc :expr xs})
 
-;;; SQL Generation
+;;; Convert to string and execute
 
-(def order-map
-  {:asc " ASC"
-   :desc " DESC"
-   nil "" })
-
-(defn order-item [x]
-  (let [order (order-map (:order x))]
-    (cond
-      (:expr x) (map #(str (name %) order) (:expr x))
-      :else (name x))))
-
-(defn order-clause [xs]
-  (when xs
-    (let [items (string/join ", " (flatten (map order-item xs)))]
-      (str " ORDER BY " items))))
-
-(defn to-sql [stmt]
-  ;; DMK TODO: turn this into multimethod based on stmt key
-  (let [{:keys [fields from where order-by limit offset]} stmt
-        flds (string/join ", " (map #(name (:field %)) fields))
-        from (when from (str " FROM " (name from)))
-        where (when where (str " WHERE " where))
-        ord (order-clause order-by)
-        lim (when limit (str " LIMIT " limit))
-        off (when offset (str " OFFSET " offset))
-        qry (str "SELECT " flds from where ord lim off ";")]
-    qry))
+(def to-sql sql/to-sql)
 
 (defn do-sql [stmt]
   (let [sql-str (if (:sql-stmt stmt) (to-sql stmt) stmt)]
---
+++ src/seaquell/to_sql.clj
@@ -0,0 +1,32 @@
+(ns seaquell.to-sql
+  (:require [clojure.string :as string]))
+
+;;; SQL Generation
+
+(def order-map
+  {:asc " ASC"
+   :desc " DESC"
+   nil "" })
+
+(defn order-item [x]
+  (let [order (order-map (:order x))]
+    (cond
+      (:expr x) (map #(str (name %) order) (:expr x))
+      :else (name x))))
+
+(defn order-clause [xs]
+  (when xs
+    (let [items (string/join ", " (flatten (map order-item xs)))]
+      (str " ORDER BY " items))))
+
+(defn to-sql [stmt]
+  ;; DMK TODO: turn this into multimethod based on stmt key
+  (let [{:keys [fields from where order-by limit offset]} stmt
+        flds (string/join ", " (map #(name (:field %)) fields))
+        from (when from (str " FROM " (name from)))
+        where (when where (str " WHERE " where))
+        ord (order-clause order-by)
+        lim (when limit (str " LIMIT " limit))
+        off (when offset (str " OFFSET " offset))
+        qry (str "SELECT " flds from where ord lim off ";")]
+    qry))
