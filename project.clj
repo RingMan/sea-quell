5ed220ef08e3b74115371831269b923ed58b7f1a
David Kettering <david.ringman@gmail.com>
2013-03-27 22:23:57 -0500

    Depend on 'diesel' via Clojars
    Depend on a deployed version of 'diesel' rather than embedded
    source code.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [diesel "0.1.0"]
                  [korma "0.3.0-RC4"]]
   :profiles {:dev {:dependencies [[midje "1.5-beta1"]
                                   [org.xerial/sqlite-jdbc "3.7.2"]
