48255003c9ac107c752fbc955a8b252598f93607
David Kettering <david.ringman@gmail.com>
2013-04-20 09:28:28 -0500

    Add SLQZoo 'Using Null'

---
+++ src/seaquell/zoo/using_null.clj
@@ -0,0 +1,68 @@
+(ns seaquell.zoo.using-null
+  (:use seaquell.core))
+
+;; The following queries are solutions (as of 4/20/2013) to the
+;; tutorial at http://sqlzoo.net/wiki/Using_Null
+;; Each query is named for the question it answers.
+;;
+;; To try them out, open a repl and issue the commands below.
+;; Submitting the resulting SQL into the tutorial should produce
+;; correct answers.
+
+(comment
+  (use 'seaquell.core)
+  (use 'seaquell.zoo.using-null)
+  (println (select$ q1)))
+
+(def q1 (select :name (from :teacher) (where [:is :dept nil])))
+
+(def q2
+  (select [:teacher.name :dept.name]
+          (from :teacher
+                (inner-join :dept (on {:teacher.dept :dept.id})))))
+
+(def q3
+  (select [:teacher.name :dept.name]
+          (from :teacher
+                (left-join :dept (on {:teacher.dept :dept.id})))))
+
+(def q4
+  (select [:teacher.name :dept.name]
+          (from :teacher
+                (right-join :dept (on {:teacher.dept :dept.id})))))
+
+(def q5
+  (select [:name [:coalesce :mobile "07986 444 2266"]] (from :teacher)))
+
+(def q6
+  (select [:teacher.name '(coalesce dept.name "None")]
+          (from :teacher
+                (left-join :dept (on {:teacher.dept :dept.id})))))
+
+(def q7 (select [[count :teacher.name] [count :mobile]] (from :teacher)))
+
+(def q8
+  (select [:dept.name [count :teacher.name]]
+          (from :teacher
+                (right-join :dept (on {:teacher.dept :dept.id})))
+          (group :dept.name)))
+
+(def q9
+  (select [:name '(cond
+                    (in dept [vals 1 2]) "Sci"
+                    :else "Art")]
+          (from :teacher)))
+
+(def q10
+  (select [:name '(cond
+                    (in dept [vals 1 2]) "Sci"
+                    (= dept 3) "Art"
+                    :else "None")]
+          (from :teacher)))
+
+(def q10-b
+  (select [:name '(cond
+                    (or (= dept 1) (= dept 2)) "Sci"
+                    (= dept 3) "Art"
+                    :else "None")]
+          (from :teacher)))
