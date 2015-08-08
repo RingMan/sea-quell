fb56f17e198cd51d51c6b704d99d0428d3b8868f
David Kettering <david.ringman@gmail.com>
2015-08-08 08:34:40 -0500

    Bump midje dependencies; add clojure profiles

--- project.clj
+++ project.clj
@@ -9,9 +9,14 @@
                  [diesel "0.1.1"]
                  [korma "0.3.0-RC4"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [midje "1.5-beta1"]
+                                  [midje "1.7.0"]
                                   [org.xerial/sqlite-jdbc "3.8.10.1"]
                                   [mysql/mysql-connector-java "5.1.23"]
                                   [org.hsqldb/hsqldb "2.2.9"]]
                    :source-paths ["dev"]
-                   :plugins [[lein-midje "3.0-beta1"]]}})
+                   :plugins [[lein-midje "3.1.3"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+  :aliases {"test-all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "midje"]
+            "check-all" ["with-profile" "1.4:1.5:1.6" "check"]})
