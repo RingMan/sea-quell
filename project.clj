03fdf8e9e23cc65af334bcf9b2e80ca1e648cb37
David Kettering <david.ringman@gmail.com>
2015-08-22 20:35:14 -0500

    Mark version as MS-specific

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject seaquell "0.2.0"
+(defproject seaquell "0.2.0-MS"
   :description "Tame the stormy seas of SQL with sea-quell, another DSL for generating SQL statements"
   :url "https://github.com/RingMan/sea-quell"
   :scm {:name "git"
