bfdf9ddc04168b80e5407d2c6f88f5c64ffc227b
David Kettering <david.ringman@gmail.com>
2020-02-19 10:27:30 -0600

    Update SQLZoo self-join answers for question 10

--- src/seaquell/zoo/self_join.clj
+++ src/seaquell/zoo/self_join.clj
@@ -111,13 +111,61 @@
           (where {:S1.name "Craiglockhart"})))
 
 (def q10
-  (select-distinct
-    [:a.name :c.name]
-    (from :stops (as :a)
-          (join :route (as :z) (on {:a.id :z.stop}))
-          (join :route (as :y) (on {:y.num :z.num}))
-          (join :stops (as :b) (on {:y.stop :b.id}))
-          (join :route (as :x) (on {:x.num :y.num}))
-          (join :stops (as :c) (on {:c.id :x.stop})))
-    (where {:a.name "Craiglockhart", :c.name "Sighthill"})))
+  (select
+    [:ra.num :ra.company :sb.name :rd.num :rd.company]
+    (from :route (as :ra)
+          (join :stops (as :sa) (on {:ra.stop :sa.id}))
+          (join :route (as :rb) (using :num :company))
+          (join :stops (as :sb) (on {:rb.stop :sb.id}))
+          (join :route (as :rc))
+          (join :stops (as :sc) (on {:rc.stop :sc.id}))
+          (join :route (as :rd) (on {:rc.num :rd.num, :rc.company :rd.company}))
+          (join :stops (as :sd) (on {:rd.stop :sd.id})))
+    (where {:sa.name "Craiglockhart", :sd.name "Lochend", :sb.name :sc.name})
+    (order-by :ra.company :ra.num :sb.name :rd.company :rd.num)))
+
+(def q10-b
+  (select
+    [:q1.num :q1.company :q1.name :q2.num :q2.company]
+    (from
+      (select
+        [:x.num :x.company :b.name]
+        (from :route (as :x)
+              (join :stops (as :a) (on {:x.stop :a.id}))
+              (join :route (as :y) (on {:x.num :y.num, :x.company :y.company}))
+              (join :stops (as :b) (on {:y.stop :b.id})))
+        (where {:a.name "Craiglockhart"}))
+      (as :q1)
+      (join
+        (select
+          [:x.num :x.company :a.name]
+          (from :route (as :x)
+                (join :stops (as :a) (on {:x.stop :a.id}))
+                (join :route (as :y) (on {:x.num :y.num, :x.company :y.company}))
+                (join :stops (as :b) (on {:y.stop :b.id})))
+          (where {:b.name "Lochend"}))
+        (as :q2)
+        (on {:q1.name :q2.name})))
+    (order-by :q1.company :q1.num :q1.name :q2.company :q2.num)))
+
+(def q10-c
+  (with
+    :dest[:company :num :pos :name] :as
+    (select [:company :num :pos :name]
+            (from :route (join :stops (on {:stop :id})))),
+    :leg[:company :num :p1 :s1 :p2 :s2] :as
+    (select
+      [:a.company :a.num :a.pos :a.name :b.pos :b.name]
+      (from :dest (as :a)
+            (join :dest (as :b) (on {:a.company :b.company
+                                     :a.num :b.num
+                                     :a.pos [not= :b.pos]})))),
+    :leg1[:company :num :p1 :s1 :p2 :s2] :as
+    (select :* (from :leg) (where {:s1 "Craiglockhart"}))
+    :leg2[:company :num :p1 :s1 :p2 :s2] :as
+    (select :* (from :leg) (where {:s2 "Lochend"}))
+    (select
+      [:leg1.num :leg1.company :leg1.s2 (as :name) :leg2.num :leg2.company]
+      (from :leg1 (join :leg2 (on {:leg1.s2 :leg2.s1})))
+      (order-by :leg1.company :leg1.num :name :leg2.company :leg2.num))))
 
