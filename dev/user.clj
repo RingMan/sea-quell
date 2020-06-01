1af300705dc78cb4f0cf605e1f4bd4bede41ad0f
David Kettering <david.kettering@navy.mil>
2020-06-01 17:33:15 -0500

    Use zprint for REPL pretty printing

--- dev/user.clj
+++ dev/user.clj
@@ -14,6 +14,11 @@
             [me.raynes.fs :as fs]
             [midje.repl :refer [autotest]]
             [clojure.java.jdbc :as jdb]
+            [zprint.config :refer [default-zprint-options get-options get-default-options
+                                   reset-default-options!  reset-options!]]
+            [zprint.core :as zp :refer [czprint czprint-fn set-options!]]
+            [zprint.cutil :refer [kcol ks->v]]
+            [zprint.rutil :refer [update-print-fn! render]]
             [diesel.core :refer [mk-map mk-map* edit] :as dsl]
             [diesel.edit :as ed]
             [seaquell.core :refer :all :rename {into s-into #_#_update s-update}]
@@ -24,6 +29,49 @@
             [seaquell.sqlite :as sq3 :refer [db-spec tables]]
             [seaquell.spec :as ss]
             [seaquell.util :as u]))
+
+(def colors #{:black :white :red :green :blue :cyan :magenta :yellow})
+
+(def zp-colors
+  {:brace :white,
+   :bracket :white,
+   :char :bright-magenta,
+   :comma :bright-white,
+   :comment :bright-black,
+   :deref :red,
+   :false :bright-magenta,
+   :fn :bright-red,
+   :hash-brace :white ,
+   :hash-paren :white,
+   :keyword :blue,
+   :nil :bright-magenta,
+   :none :white,
+   :number :bright-magenta,
+   :paren :white,
+   :quote :bright-yellow,
+   :regex :bright-cyan,
+   :string :bright-green,
+   :symbol :bright-white,
+   :syntax-quote :bright-yellow,
+   :syntax-quote-paren :white,
+   :true :bright-magenta,
+   :uneval :bright-red,
+   :unquote :bright-yellow,
+   :unquote-splicing :bright-yellow,
+   :user-fn :bright-yellow})
+
+(def zp-opt
+  {:color-map zp-colors
+   :map {:comma? true
+         :indent 2
+         :key-order [:id :name :tag]
+         :key-color (kcol (ks->v [:id :name :tag] [:bold]) zp-colors)}
+   :vector {:wrap-after-multi? false}
+   :uneval {:color-map zp-colors}})
+
+(set-options! zp-opt)
+
+(update-print-fn! render)
 
 (def q1 (select ['(avg Price) :as :AvgPrice '(count Price) :as :CntPrice] (from :cars)))
 
--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/RingMan/sea-quell"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["jitpack" "https://jitpack.io"]]
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
                                     :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.10.1"]
@@ -19,7 +20,8 @@
                    :plugins [[lein-cloverage "1.1.2"]
                              [lein-midje "3.2.2"]]}
              :repl {:dependencies [[expound "0.8.4"]
-                                   [integrant/repl "0.3.1"]]
+                                   [integrant/repl "0.3.1"]
+                                   [com.github.RingMan/zprint "dev-SNAPSHOT"]]
                     :source-paths ["dev"]}
              :test {:dependencies [[clj-commons/fs "1.5.2"]
                                    [midje "1.9.9"]
