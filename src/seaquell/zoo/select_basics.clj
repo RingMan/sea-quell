9eb499a41b43245d5c6b2bd4372129ede3669502
David Kettering <david.kettering@navy.mil>
2020-07-22 08:44:03 -0500

    Update SQLZoo select basics answers

--- src/seaquell/zoo/select_basics.clj
+++ src/seaquell/zoo/select_basics.clj
@@ -3,7 +3,7 @@
     :exclude [alter distinct drop group-by into set update partition-by when])
   (:require [seaquell.core :refer :all]))
 
-;; The following queries are solutions (as of 4/19/2013) to the
+;; The following queries are solutions (as of 7/21/2020) to the
 ;; tutorial at http://sqlzoo.net/wiki/SELECT_basics
 ;; Each query is named for the question it answers.
 ;;
@@ -16,23 +16,13 @@
   (use 'seaquell.zoo.select-basics)
   (println (select$ q1)))
 
-(def q1 (select :population (from :bbc) (where {:name "Germany"})))
+(def q1 (select :population (from :world) (where {:name "Germany"})))
 
-(def q2 (select [:name [/ :gdp :population]]
-                (from :bbc) (where [> :area 5000000])))
-
-(def q3 (select [:name :region]
-                (from :bbc)
-                (where {:area [< 2000]
-                        :gdp [> 5000000000]})))
-
-(def q4
+(def q2
   (select
-    [:name :population] (from :bbc)
-    (where [:in :name [vals "Denmark" "Finland" "Norway" "Sweden"]])))
+    [:name :population] (from :world)
+    (where [:in :name [vals "Denmark" "Norway" "Sweden"]])))
 
-(def q5 (select :name (from :bbc) (where [:like :name "G%"])))
-
-(def q6 (select [:name [/ :area 1000]] (from :bbc)
-                (where [:between :area 207600 244820])))
+(def q3 (select [:name :area] (from :world)
+                (where [:between :area 200000 250000])))
 
