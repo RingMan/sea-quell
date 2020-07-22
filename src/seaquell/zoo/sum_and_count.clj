fc64154ae52934effb36d1d31109f03f3f9f6e1f
David Kettering <david.kettering@navy.mil>
2020-07-22 13:32:15 -0500

    Update SQLZoo sum and count answers

--- src/seaquell/zoo/sum_and_count.clj
+++ src/seaquell/zoo/sum_and_count.clj
@@ -3,7 +3,7 @@
     :exclude [alter distinct drop group-by into set update partition-by when])
   (:require [seaquell.core :refer :all]))
 
-;; The following queries are solutions (as of 4/19/2013) to the
+;; The following queries are solutions (as of 7/21/2020) to the
 ;; tutorial at http://sqlzoo.net/wiki/SUM_and_COUNT
 ;; Each query is named for the question it answers.
 ;;
@@ -14,32 +14,32 @@
 (comment
   (use 'seaquell.core)
   (use 'seaquell.zoo.sum-and-count)
-  (println (select$ q1)))
+  (select$ q1))
 
-(def q1 (select [[:sum :population]] (from :bbc)))
+(def q1 (select [[:sum :population]] (from :world)))
 
-(def q2 (select-distinct :region (from :bbc)))
+(def q2 (select-distinct :continent (from :world)))
 
 (def q3
-  (select [[:sum :gdp]] (from :bbc) (where {:region "Africa"})))
+  (select [[:sum :gdp]] (from :world) (where {:continent "Africa"})))
 
 (def q4
-  (select [[count :name]] (from :bbc) (where [>= :area 1000000])))
+  (select [[count :name]] (from :world) (where [>= :area 1000000])))
 
 (def q5
   (select [[:sum :population]]
-          (from :bbc)
-          (where [:in :name [vals "France" "Germany" "Spain"]])))
+          (from :world)
+          (where [:in :name [vals "Estonia" "Latvia" "Lithuania"]])))
 
 (def q6
-  (select [:region [count :name]] (from :bbc) (group-by :region)))
+  (select [:continent [count :name]] (from :world) (group-by :continent)))
 
 (def q7
-  (select [:region [count :name]]
-          (from :bbc)
+  (select [:continent [count :name]]
+          (from :world)
           (where [>= :population 10000000])
-          (group-by :region)))
+          (group-by :continent)))
 
 (def q8
-  (select :region (from :bbc) (group-by :region)
+  (select :continent (from :world) (group-by :continent)
           (having [>= [:sum :population] 100000000])))
