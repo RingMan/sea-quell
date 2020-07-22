023a8c72838a3ccce039e0bb454990f463cf127b
David Kettering <david.kettering@navy.mil>
2020-07-22 11:23:04 -0500

    Update SQLZoo select from nobel answers

--- src/seaquell/zoo/select_from_nobel.clj
+++ src/seaquell/zoo/select_from_nobel.clj
@@ -3,7 +3,7 @@
     :exclude [alter distinct drop group-by into set update partition-by when])
   (:require [seaquell.core :refer :all]))
 
-;; The following queries are solutions (as of 4/19/2013) to the
+;; The following queries are solutions (as of 7/21/2020) to the
 ;; tutorial at http://sqlzoo.net/wiki/SELECT_from_Nobel_Tutorial
 ;; Each query is named for the question it answers.
 ;;
@@ -14,7 +14,7 @@
 (comment
   (use 'seaquell.core)
   (use 'seaquell.zoo.select-from-nobel)
-  (println (select$ q1)))
+  (select$ q1))
 
 (def q1 (select [:yr :subject :winner] :from :nobel :where {:yr 1950}))
 
@@ -36,24 +36,51 @@
   (select :* (from :nobel)
           (where '(in winner (vals "Theodore Roosevelt"
                                    "Woodrow Wilson"
-                                   "Jed Bartlet"
-                                   "Jimmy Carter")))))
+                                   "Jimmy Carter"
+                                   "Barack Obama")))))
 
 (def q7 (select :winner :from :nobel :where [:like :winner "John %"]))
 
 (def q8
-  (select-distinct
-    :yr (from :nobel)
-    (where {:subject "Physics"
-            :yr [:not-in (select :yr :from :nobel
-                                 (where {:subject "Chemistry"}))]})))
+  (select
+    :* (from :nobel)
+    (where '(or {subject "Physics", yr 1980}
+                {subject "Chemistry", yr 1984}))))
 
-(def q8-b
-  (select-distinct
-    :yr
-    (from (select [:yr :subject] (from :nobel)
-                  (where {:subject "Physics"})) (as :n1)
-          (left-join (select [:yr :subject] (from :nobel)
-                        (where {:subject "Chemistry"})) (as :n2)
-                     (using :yr)))
-    (where '(is n2.subject nil))))
+(def q9
+  (select
+    :* (from :nobel)
+    (where {:yr 1980, :subject [:not-in [vals "Chemistry" "Medicine"]]})))
+
+(def q10
+  (select
+    :* (from :nobel)
+    (where '(or {subject "Medicine", yr [< 1910]}
+                {subject "Literature", yr [>= 2004]}))))
+
+(def q11
+  "If the umlaut doesn't print properly at the REPL, you may need to change
+  the encoding to match the UTF-8 encoding of this file. On a Windows 10
+  machine, changing the code page by entering `chcp 65001` before starting
+  the REPL does the trick."
+  (select
+    :* (from :nobel)
+    (where {:winner [:in [val "Peter Grünberg"]]})))
+
+(def q12
+  (select
+    :* (from :nobel)
+    (where {:winner [:in [val "Eugene O''Neill"]]})))
+
+(def q13
+  (select
+    [:winner :yr :subject] (from :nobel)
+    (where {:winner [:like "Sir%"]})
+    (order-by (desc :yr) :winner)))
+
+(def q14
+  (select
+    [:winner :subject] (from :nobel)
+    (where {:yr 1984})
+    (order-by [:in :subject [vals "Physics" "Chemistry"]] :subject :winner)))
+
