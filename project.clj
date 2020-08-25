6d6f087b0e7ce6111991a46e2eae9069f60252ee
David Kettering <david.kettering@navy.mil>
2020-08-25 16:04:17 -0500

    Bump project to release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject seaquell "0.5.0-SNAPSHOT"
+(defproject seaquell "0.5.0"
   :description "Tame the stormy seas of SQL with sea-quell, another DSL for generating SQL statements"
   :url "https://github.com/RingMan/sea-quell"
   :license {:name "Eclipse Public License"
